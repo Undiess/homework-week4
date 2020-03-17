7040f25872371ad91bbb1371603e408ef6ad68d8
Andy Nguyen <nguyen.andy67@gmail.com>
2020-03-17 22:47:31 +1030

    Added firt q

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
+var questions = $("#question-text")
+var startbutton = $("#start-button")
+var buttondiv = $("#buttondiv")
+function startQuiz()
+{ 
+startbutton.remove();
+questions.text("Question 1: How many woods did the wood chuck chuck when the wood chuck chuck some wood?")
+
+}
 
 
 
 
-
-
-$("start-button").on("click",function(){
+$("#start-button").on("click",function(){
+    startQuiz()
+   
 })
+3
